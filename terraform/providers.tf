7170af0147847b2299d0fb059e82fc6b1c547506
Brandon H <hurlburb@microsoft.com>
2020-01-16 09:55:01 -0800

    add backend storage for terraform to avoid warning during build

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -3,3 +3,7 @@ provider "azurerm" {
   # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
   version = "~> 1.0"
 }
+
+terraform {
+  backend "azure" {}
+}
