c5b9616cd86895faee8b45e34b74629de4a28a5b
Brandon H <hurlburb@microsoft.com>
2020-01-16 13:22:48 -0800

    Update initial event grid function code to do publishing

--- Terraform.Functions/NotificationHandlerFunctions.cs
+++ Terraform.Functions/NotificationHandlerFunctions.cs
@@ -1,20 +1,18 @@
 // Default URL for triggering event grid function in the local environment.
 // http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
-using System;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.EventGrid.Models;
+using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Extensions.Logging;
 
 namespace Terraform.Functions
 {
-    public static class NotificationHandler
+    public static class NotificationHandlerFunctions
     {
-        [FunctionName("NotificationHandler")]
-        public static void Run([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log)
+        [FunctionName(nameof(NotificationHandler))]
+        public static void NotificationHandler([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log)
         {
-            log.LogInformation(eventGridEvent.Data.ToString());
+            log.LogInformation($@"Received notification! {eventGridEvent.Data}");
         }
     }
 }
---
+++ Terraform.Functions/StorageEventHandlerFunctions.cs
@@ -0,0 +1,20 @@
+// Default URL for triggering event grid function in the local environment.
+// http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
+using Microsoft.Azure.EventGrid.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Extensions.Logging;
+
+namespace Terraform.Functions
+{
+    public static class StorageEventHandlerFunctions
+    {
+        [FunctionName(nameof(StorageEventHandler))]
+        public static void StorageEventHandler([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log, [EventGrid(TopicEndpointUri = @"%EGNotificationTopic%", TopicKeySetting = @"EGNotificationTopicKey")]out string notificationMessage)
+        {
+            log.LogInformation(eventGridEvent.Data.ToString());
+
+            notificationMessage = $@"Blob event received: {eventGridEvent.Subject} - {eventGridEvent.Data}";
+        }
+    }
+}
